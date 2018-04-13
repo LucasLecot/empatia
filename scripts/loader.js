b5a5e8f77ab14ac1ca0e7f0258d264cd968cd4e3
lucas lecot <lucas@jeveuxjaurai.com>
2018-04-13 20:16:23 +0200

    correction

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,11 +1,36 @@
 const loader = document.querySelector('.loader-pill');
-const content = document.querySelector('.content');
+
 const load = document.querySelector('.loader');
 
 setTimeout( () => {
     loader.style.opacity = "0";
-    content.style.opacity = "1";
     loader.style.display = "none";
     loader.remove();
     load.classList.remove("loader");
-}, 100);
+}, 8000);
+
+
+function typeEffect(element, speed) {
+    var text = $(element).text();
+    $(element).html('');
+
+    var i = 0;
+    var timer = setInterval(function() {
+        if (i < text.length) {
+            $(element).append(text.charAt(i));
+            i++;
+        } else {
+            clearInterval(timer);
+        }
+    }, speed);
+}
+
+$( document ).ready(function() {
+    var speed = 100;
+    var delay = $('#dystopieH2').text().length * speed + speed;
+    typeEffect($('#dystopieH2'), speed);
+    setTimeout(function(){
+        $('#dystopieP').css('display', 'inline-block');
+        typeEffect($('#dystopieP'), speed);
+    }, delay);
+});
